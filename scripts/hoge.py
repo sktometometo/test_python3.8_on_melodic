1e7c928b9f98dfa0a481ae51eac78ed30d422587
Your Name <you@example.com>
2024-09-11 14:47:30 +0900

    update

--- scripts/hoge.py
+++ scripts/hoge.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import rospy
 import os
 import sys
+print(f"original PYTHONPATH:{sys.path}")
 sys.path.remove('/opt/ros/{}/lib/python2.7/dist-packages'.format(os.getenv('ROS_DISTRO')))
 sys.path.append('/opt/ros/{}/lib/python3/dist-packages'.format(os.getenv('ROS_DISTRO')))
+print(f"modified PYTHONPATH:{sys.path}")
 
 import PyKDL
 from cv_bridge import CvBridge
